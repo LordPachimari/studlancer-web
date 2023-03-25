2eaf2e30b4e8cd8781483759b04a8a2b2300ef3d
LordPachimari <opachimari@gmail.com>
2023-03-25 21:18:34 +1100

    es lint hell

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -19,7 +19,8 @@ const config = {
     project: path.join(__dirname, "tsconfig.json"),
   },
   plugins: ["@typescript-eslint"],
-  extends: ["next/core-web-vitals", "plugin:@typescript-eslint/recommended"],
+  // extends: ["next/core-web-vitals", "plugin:@typescript-eslint/recommended"],
+  extends: "next",
   // rules: {
   //   "@typescript-eslint/consistent-type-imports": [
   //     "off",
